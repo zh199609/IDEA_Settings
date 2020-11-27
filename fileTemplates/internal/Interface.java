0d2d5d9bd8252e17731832fc6959770eee813e89
albertzh <Password02!>
2020-11-27 16:27:47 +0800

    IU-2020.2.3 <albertzh@DESKTOP-T2ERR9A Create baseRefactoring.xml, ide.general.xml, project.default.xml, Enum.java, Interface.java, lombok-plugin.xml, shared-indexes.xml, Default.xml, find.xml, filetypes.xml, keymap.xml, Java.xml, colors.scheme.xml, KeyPromoterXSettings.xml, databaseDrivers.xml, templates.xml, javarebel.xml, Windows Proper Redo.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, lightEdit.xml, wsl.distributions.xml, encoding.xml, vcs.xml, toStringTemplates.xml, mybatis.xml, editor.xml, methodDesc.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,13 @@
+/*
+ * CMB Confidential
+ *
+ * Copyright (C) 2020 China Merchants Bank Co., Ltd. All rights reserved.
+ *
+ * No part of this file may be reproduced or transmitted in any form or by any
+ * means, electronic, mechanical, photocopying, recording, or otherwise, without
+ * prior written permission of China Merchants Bank Co., Ltd.
+ */
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+public interface ${NAME} {
+}
